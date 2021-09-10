b5b71cb8284bc26cb65649b04aac4d2a0eba8b32
zzzj <1152092752@qq.com>
2021-09-10 19:17:39 +0800

    add compare yaml

--- src/com/github/zzzj1233/extension/WrapperMap.kt
+++ src/com/github/zzzj1233/extension/WrapperMap.kt
@@ -8,13 +8,23 @@ class WrapperMap(private val origin: Map<String, Any>, private val alternative: 
     override fun get(key: String): Any? {
         val value = origin[key]
 
-        val result = value ?: alternative[key]
+        if (value != null && value is Map<*, *> && value !is WrapperMap) {
+            if (alternative[key] is Map<*, *>) {
+                return WrapperMap(value as Map<String, Any>, alternative[key] as Map<String, Any>)
+            }
+        }
 
         return value ?: alternative[key]
     }
 
     override fun getOrDefault(key: String, defaultValue: Any): Any {
         val value = origin[key]
+
+        if (value != null && value is Map<*, *> && value !is WrapperMap) {
+            if (alternative[key] is Map<*, *>) {
+                return WrapperMap(value as Map<String, Any>, alternative[key] as Map<String, Any>)
+            }
+        }
 
         return value ?: alternative.getOrDefault(key, defaultValue)
     }
